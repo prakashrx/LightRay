1ec28486c23d013618f97bf929975a8d64841790
Prakash R <git@prakashrx.com>
2023-07-04 23:27:28 +0800

    feat: added basic image and window setup

--- LightRay/premake5.lua
+++ LightRay/premake5.lua
@@ -1,4 +1,4 @@
-project "WalnutApp"
+project "LightRay"
    kind "ConsoleApp"
    language "C++"
    cppdialect "C++17"
---
+++ LightRay/src/LightRay.cpp
@@ -0,0 +1,75 @@
+#include "Walnut/Application.h"
+#include "Walnut/EntryPoint.h"
+
+#include "Walnut/Image.h"
+#include "Walnut/Random.h"
+#include "Walnut/Timer.h"
+
+class ExampleLayer : public Walnut::Layer
+{
+public:
+	virtual void OnUIRender() override
+	{
+		ImGui::Begin("Settings");
+		ImGui::Text("Render Time : %.3fms", m_LastRenderTime);
+		if (ImGui::Button("Render")) {
+			Render();
+		}
+		ImGui::End();
+
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{ 0.0f, 0.0f });
+		ImGui::Begin("ViewPort");
+		auto region = ImGui::GetContentRegionAvail();
+		m_ViewPortHeight = region.y, m_ViewPortWidth = region.x;
+
+		if (m_Image) {
+			ImGui::Image(m_Image->GetDescriptorSet(), { (float)m_Image->GetWidth(), (float)m_Image->GetHeight() });
+		}
+		ImGui::End();
+		ImGui::PopStyleVar();
+
+		Render();
+	}
+
+	void Render() {
+		Walnut::Timer timer;
+		if (!m_Image || m_ViewPortWidth != m_Image->GetWidth() || m_ViewPortHeight != m_Image->GetHeight()) {
+			m_Image = std::make_shared<Walnut::Image>(m_ViewPortWidth, m_ViewPortHeight, Walnut::ImageFormat::RGBA);
+			delete[] m_ImageData;
+			m_ImageData = new uint32_t[m_ViewPortHeight * m_ViewPortWidth];
+		}
+
+		for (uint32_t i{ 0 }; i < m_ViewPortHeight * m_ViewPortWidth; i++) {
+			m_ImageData[i] = Walnut::Random::UInt() | 0xff000000;
+		}
+		m_Image->SetData(m_ImageData);
+		m_LastRenderTime = timer.ElapsedMillis();
+	}
+
+private:
+	std::shared_ptr<Walnut::Image> m_Image;
+	uint32_t* m_ImageData = nullptr;
+	uint32_t m_ViewPortWidth = 0, m_ViewPortHeight = 0;
+	float m_LastRenderTime = 0;
+};
+
+Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
+{
+	Walnut::ApplicationSpecification spec;
+	spec.Name = "Walnut Example";
+
+	Walnut::Application* app = new Walnut::Application(spec);
+	app->PushLayer<ExampleLayer>();
+	//app->SetMenubarCallback([app]()
+	//{
+	//	if (ImGui::BeginMenu("File"))
+	//	{
+	//		if (ImGui::MenuItem("Exit"))
+	//		{
+	//			app->Close();
+	//		}
+	//		ImGui::EndMenu();
+	//	}
+	//});
+	return app;
+}
